e7bb0da3852784b08286c6f847daaeada7fe37bb
alexwitt23 <awitt2399@utexas.edu>
2021-04-20 11:30:04 -0500

    adding some files from class

---
+++ homework07/src/api.py
@@ -0,0 +1,24 @@
+import json
+
+import flask
+from flask import request
+
+from . import jobs
+
+app = flask.Flask(__name__)
+
+
+@app.route("/jobs", methods=["POST"])
+def jobs_api():
+    try:
+        job = request.get_json(force=True)
+    except Exception as e:
+        return (
+            True,
+            json.dumps({"status": "Error", "message": "Invalid JSON: {}.".format(e)}),
+        )
+    return json.dumps(jobs.add_job(job["start"], job["end"]))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
